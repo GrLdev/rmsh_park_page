c485e30efe5085664170c8c82439c074211b2e4e
Toby Nott <tobynott80@gmail.com>
2023-05-23 22:44:24 +0100

    typo error

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,4 @@
-<h1>Rate My Studen Home - Cardiff</h1>
+<h1>Rate My Student Home - Cardiff</h1>
 <h2>Coming Soon!</h2>
 <p>
     Discover the Best Student Homes in Cardiff: Rate, Review, and Find Your
