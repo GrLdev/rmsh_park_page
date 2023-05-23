85f5c04fc3ca9f8b5c6e70bd56c16ef18d99a822
Toby Nott <tobynott80@gmail.com>
2023-05-23 22:43:07 +0100

    added tagline

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Rate My Studen Home - Cardiff</h1>
+<h2>Coming Soon!</h2>
+<p>
+    Discover the Best Student Homes in Cardiff: Rate, Review, and Find Your
+    Perfect Home!
+</p>
