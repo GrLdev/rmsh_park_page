a7cf6d703dfe4e6fd0178a05249af8f6dceb6a1a
Toby Nott <tobynott80@gmail.com>
2023-05-24 23:18:40 +0100

    got rid of unnecessary packages and imports

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,14 +1,14 @@
 
-import { addContact } from './contactSender.js';
+// import { addContact } from './contactSender.js';
 
 
-export const actions = {
-    default: async ({ request }) => {
-        const data = await request.formData();
-        const email = data.get('email');
-        console.log(email + ' - email retrieved via POST');
-        if (email != null) {
-            await addContact(email.toString());
-        }
-    },
-};  
+// export const actions = {
+//     default: async ({ request }) => {
+//         const data = await request.formData();
+//         const email = data.get('email');
+//         console.log(email + ' - email retrieved via POST');
+//         if (email != null) {
+//             await addContact(email.toString());
+//         }
+//     },
+// };  
--- src/routes/emailSender.js
+++ src/routes/emailSender.js
@@ -1,25 +1,25 @@
-import sgMail from '@sendgrid/mail';
-import { SENDGRID_API_KEY } from '$env/static/private';
+// import sgMail from '@sendgrid/mail';
+// import { SENDGRID_API_KEY } from '$env/static/private';
 
-sgMail.setApiKey(SENDGRID_API_KEY);
+// sgMail.setApiKey(SENDGRID_API_KEY);
 
-/**
- * @param {{ toString: () => any; }} email
- */
-export async function sendEmail(email) {
-    const msg = {
-      to: email?.toString(),
-      from: 'toby@ratemystudenthome.com',
-      subject: 'Test Subject',
-      text: 'Test Text',
-      html: '<strong>test html</strong>',
-    };
+// /**
+//  * @param {{ toString: () => any; }} email
+//  */
+// export async function sendEmail(email) {
+//     const msg = {
+//       to: email?.toString(),
+//       from: 'toby@ratemystudenthome.com',
+//       subject: 'Test Subject',
+//       text: 'Test Text',
+//       html: '<strong>test html</strong>',
+//     };
   
-    try {
-      await sgMail.send(msg);
-      console.log('Email sent successfully');
-    } catch (error) {
-      console.error('Error sending email:', error);
-    }
-  }
+//     try {
+//       await sgMail.send(msg);
+//       console.log('Email sent successfully');
+//     } catch (error) {
+//       console.error('Error sending email:', error);
+//     }
+//   }
   
