343151073d18dfcf673d9d40d8f874ce6914ad8d
Toby Nott <tobynott80@gmail.com>
2023-05-24 20:24:16 +0100

    refactored email send into different componant

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,31 +1,14 @@
 
-import sgMail from '@sendgrid/mail';
+import { sendEmail } from './emailSender.js';
 
-import { SENDGRID_API_KEY } from '$env/static/private'
-
-sgMail.setApiKey(SENDGRID_API_KEY)
 
 export const actions = {
     default: async ({ request }) => {
         const data = await request.formData();
         const email = data.get('email');
-        console.log(email + " - email retrieved via POST");
-
-        const msg = {
-            to: email?.toString(),
-            from: 'info@ratemystudenthome.com',
-            subject: 'Test Subject',
-            text: 'Test Text',
-            html: '<strong>test html</strong>',
-        };
-
-        sgMail
-            .send(msg)
-            .then(() => {
-                console.log('Email sent')
-            })
-            .catch((error) => {
-                console.error(error)
-            })
+        console.log(email + ' - email retrieved via POST');
+        if (email != null) {
+            await sendEmail(email.toString());
+        }
     },
-};
+};  
---
+++ src/routes/emailSender.js
@@ -0,0 +1,25 @@
+import sgMail from '@sendgrid/mail';
+import { SENDGRID_API_KEY } from '$env/static/private';
+
+sgMail.setApiKey(SENDGRID_API_KEY);
+
+/**
+ * @param {{ toString: () => any; }} email
+ */
+export async function sendEmail(email) {
+    const msg = {
+      to: email?.toString(),
+      from: 'info@ratemystudenthome.com',
+      subject: 'Test Subject',
+      text: 'Test Text',
+      html: '<strong>test html</strong>',
+    };
+  
+    try {
+      await sgMail.send(msg);
+      console.log('Email sent successfully');
+    } catch (error) {
+      console.error('Error sending email:', error);
+    }
+  }
+  
