80a9da3f80e3288674555b36354373dd7f43f42f
Toby Nott <tobynott80@gmail.com>
2023-05-24 20:20:24 +0100

    added email send logic

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,17 +1,31 @@
 
-const sgMail = require('@sendgrid/mail')
+import sgMail from '@sendgrid/mail';
 
 import { SENDGRID_API_KEY } from '$env/static/private'
 
 sgMail.setApiKey(SENDGRID_API_KEY)
 
-/** @type {import('./$types').Actions} */
 export const actions = {
-    default: async ({request}) => {
-        // TODO: Verify email
+    default: async ({ request }) => {
         const data = await request.formData();
-        const email = data.get('email')
-        console.log(email + " - email submitted")
+        const email = data.get('email');
+        console.log(email + " - email retrieved via POST");
 
-    }
+        const msg = {
+            to: email?.toString(),
+            from: 'info@ratemystudenthome.com',
+            subject: 'Test Subject',
+            text: 'Test Text',
+            html: '<strong>test html</strong>',
+        };
+
+        sgMail
+            .send(msg)
+            .then(() => {
+                console.log('Email sent')
+            })
+            .catch((error) => {
+                console.error(error)
+            })
+    },
 };
